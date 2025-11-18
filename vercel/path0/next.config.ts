b37577a71959a772c4d5e96d80bef3e7592b7b59
Thariq <attendrys@gmail.com>
2025-11-18 10:35:38 +0000

    16:03:00.488 Running build in Washington, D.C., USA (East) – iad1 16:03:

---
+++ vercel/path0/next.config.ts
@@ -0,0 +1,34 @@
+import type { NextConfig } from 'next';
+import nextPWA from 'next-pwa';
+
+const withPWA = nextPWA({
+  dest: 'public',
+  register: true,
+  skipWaiting: true,
+  disable: process.env.NODE_ENV === 'development',
+  sw: 'sw.js',
+  importScripts: ['/firebase-messaging-sw.js'], // Correct way to import the script
+});
+
+const nextConfig: NextConfig = {
+  reactStrictMode: false,
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  images: {
+    remotePatterns: [
+      { protocol: 'https', hostname: 'placehold.co' },
+      { protocol: 'https', hostname: 'api.qrserver.com' },
+      { protocol: 'https', hostname: 'picsum.photos' },
+      { protocol: 'https', hostname: 'storage.googleapis.com' },
+      { protocol: 'https', hostname: 'res.cloudinary.com' },
+    ],
+  },
+};
+
+const config = process.env.TURBOPACK ? nextConfig : withPWA(nextConfig);
+
+export default config;
